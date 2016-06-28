3fc929fb39da0334b74c4b38d4acfc351b4ff838
smoon <smoon@csail.mit.edu>
2016-06-28 16:26:22 -0700

    main files for buyancy control unit

---
+++ fish/mbed/LPCExpress/buoyancy_test_unit/main.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+#include "BTU.h"
+
+
+Serial      pc(USBTX, USBRX);
+//AnalogIn    enc1(p16);
+//AnalogIn    enc2(p17);
+//DigitalOut  myled1(LED1);
+//DigitalOut  myled2(LED2);
+//BusOut      unused(p15,p18,p19,p20);  // Make unused analog pin to DigitalOut
+
+//QEI wheel (p16, p17, NC, 1786); //add: ,QEI::X4_ENCODING
+
+int main() 
+{
+    float interval=0.1; //interval between each PID iteration
+    
+    BTU btuobj;
+    while(1)
+    {
+        btuobj.Run(1,0.1);
+        wait(interval);
+    }
+    return 0;
+}
