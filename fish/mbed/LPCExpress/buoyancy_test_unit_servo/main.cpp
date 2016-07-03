7951b476ca60dbb633a2c12de726a26bdad0bb36
Robert Katzschmann <rkk@csail.mit.edu>
2016-07-03 19:08:38 -0400

    release mode copy over mbed program

--- fish/mbed/LPCExpress/buoyancy_test_unit_servo/main.cpp
+++ fish/mbed/LPCExpress/buoyancy_test_unit_servo/main.cpp
@@ -1,11 +1,13 @@
 #include "BTU/btu.h"
 #include "mbed.h"
 
-#define NUM_FLOATS 4
+#define NUM_FLOATS 5
 #define TIMESTEP 0.05
-// MODSERIAL pcSerial(USBTX,USBRX); //serial device
 
-Serial pc(USBTX, USBRX);
+#include "MODSERIAL.h"
+#include "SerialComm.h"
+
+// MODSERIAL pcSerial(USBTX,USBRX); //serial device
 
 AnalogIn pot1(p15);
 AnalogIn pot2(p19);
@@ -21,7 +23,7 @@ int mode = 2; //default
 float Kc = 0.0005;
 float TauI = 0.00003;
 float TauD = 0.000000000;
-float setVal = 90;
+float setVal = +90.0;
 
 void runLED() {
   counter = (counter + 1) % 20;
@@ -33,27 +35,40 @@ void runLED() {
 }
 
 int main() {
-	// MODSERIAL* pcSerial = new MODSERIAL(USBTX,USBRX); //dynamic allocation of the serial device
-	// pcSerial->printf("Start!\n");
+	MODSERIAL* pcSerial = new MODSERIAL(USBTX,USBRX); //dynamic allocation of the serial device
+	pcSerial->printf("Start!\n");
 
 	//Set up a serial communication object
-    // SerialComm serialComm(pcSerial);
-    pc.printf("Start!\r\n");
+    SerialComm serialComm(pcSerial);
 
 	//BTU m_BTU single instance is made in .h .cpp files;
-	//int mode = 2; //default
-	//float Kc = 0.0005;
-	//float TauI = 0.00003;
-	//float TauD = 0.000000000;
-	//float setVal = 90;
+    m_BTU.init();
+
     Ticker timer;
     timer.attach(&runLED, TIMESTEP);
     TestLED = 0;
+    float valueFloats[NUM_FLOATS];
 
-    while(1) {
-        pc.printf("m:%d, Kc:%f, TI:%f, TD:%f, S:%.2f, C:%2f, d: %.2f\r\n", m_BTU.m_mode, m_BTU.m_kc, m_BTU.m_taud, m_BTU.m_setval, m_BTU.m_currentval, m_BTU.m_output*100);
-        wait_ms(1000);
-        TestLED2 = !TestLED2;
-    }
+	while (1) {
+		pcSerial->printf("m:%d, Kc:%f, TI:%f, TD:%f, S:%.2f, C:%.2f, d: %.2f\r\n",
+				m_BTU.m_mode, m_BTU.m_kc, m_BTU.m_taui, m_BTU.m_taud, m_BTU.m_setval,
+				m_BTU.m_currentval, m_BTU.m_output * 100);
+		if (serialComm.checkIfNewMessage()) {
+
+			serialComm.getFloats(valueFloats, NUM_FLOATS);
+			// printing out for debugging
+			for (int i = 0; i < NUM_FLOATS; i++) {
+				pcSerial->printf("Value#%d: %f\n", i, valueFloats[i]);
+			}
+
+			mode = (int) valueFloats[0];
+			Kc = valueFloats[1];
+			TauI = valueFloats[2];
+			TauD = valueFloats[3];
+			setVal = valueFloats[4];
+		}
+		wait_ms(1000);
+		TestLED2 = !TestLED2;
+	}
 
 }
