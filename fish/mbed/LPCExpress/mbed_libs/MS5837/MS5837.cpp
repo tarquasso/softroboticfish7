e6e68880f1a1a6b4dddfb88cebcdd471bc659894
Robert Katzschmann <rkk@csail.mit.edu>
2016-07-04 15:04:48 -0400

    remove waits from MS5837 pressure unit library

--- fish/mbed/LPCExpress/mbed_libs/MS5837/MS5837.cpp
+++ fish/mbed/LPCExpress/mbed_libs/MS5837/MS5837.cpp
@@ -10,17 +10,31 @@ void MS5837::MS5837Init(void)
 {
     MS5837Reset();
     MS5837ReadProm();
+    step = 1;
+
     return;
+}
+
+void MS5837::MS5837Start(void)
+{
+	if(!started)
+	{
+		MS5837::Barometer_MS5837();
+	}
+	return;
 }
 
 /* Send soft reset to the sensor */
 void MS5837::MS5837Reset(void)
 {
+	timeOut1.detach();
+	started = false;
     /* transmit out 1 byte reset command */
     ms5837_tx_data[0] = ms5837_reset;
     if ( i2c.write( device_address,  ms5837_tx_data, 1 ) );
     //printf("send soft reset");
     wait_ms(20);
+    timeOut1.detach(); // to be sure it was detached
 }
 
 /* read the sensor calibration data from rom */
@@ -53,8 +67,6 @@ void MS5837:: MS5837ConvertD2(void)
 /* Read the previous started conversion results */
 int32_t MS5837::MS5837ReadADC(void)
 {
-    int32_t adc;
-    wait_ms(150);
     ms5837_tx_data[0] = ms5837_ADCread;
     if ( i2c.write( device_address,  ms5837_tx_data, 1 ) );
     if ( i2c.read( device_address,  ms5837_rx_data, 3 ) );
@@ -73,28 +85,41 @@ float MS5837::MS5837_Temperature (void)
 }
 
 /* Sensor reading and calculation procedure */
-void MS5837::Barometer_MS5837(void)
-{
-    int32_t dT, temp;
-    int64_t OFF, SENS, press;
+void MS5837::Barometer_MS5837(void) {
+	started = true;
+	switch (step) {
+	case 1:
+		MS5837ConvertD1();             // start pressure conversion
+		step++;
+		timeOut1.attach_us(this, &MS5837::Barometer_MS5837, ms5837_conv_time_us);
+		break;
+	case 2:
+		D1 = MS5837ReadADC(); // read the pressure value
+		MS5837ConvertD2(); // start temperature conversion
+		step++;
+		timeOut1.attach_us(this, &MS5837::Barometer_MS5837, ms5837_conv_time_us);
+		break;
+	case 3:
+		D2 = MS5837ReadADC();         // read the temperature value
+		/* calculation according MS5837-01BA data sheet DA5837-01BA_006 */
+		dT = D2 - (C[5] * 256);
+		OFF = (int64_t) C[2] * (1 << 16)
+				+ ((int64_t) dT * (int64_t) C[4]) / (1 << 7);
+		SENS = (int64_t) C[1] * (1 << 15)
+				+ ((int64_t) dT * (int64_t) C[3]) / (1 << 8);
 
-    //no need to do this everytime!
-    //MS5837Reset();                 // reset the sensor
-    //MS5837ReadProm();             // read the calibration values
-    
-    
-    MS5837ConvertD1();             // start pressure conversion
-    D1 = MS5837ReadADC();        // read the pressure value
-    MS5837ConvertD2();             // start temperature conversion
-    D2 = MS5837ReadADC();         // read the temperature value
+		temp = 2000 + (dT * C[6]) / (1 << 23);
+		T_MS5837 = (float) temp / 100.0f; // result of temperature in deg C in this var
+		press = (((int64_t) D1 * SENS) / (1 << 21) - OFF) / (1 << 13);
+		P_MS5837 = (float) press / 10.0f; // result of pressure in mBar in this var
 
-    /* calculation according MS5837-01BA data sheet DA5837-01BA_006 */
-    dT       = D2 - (C[5]* 256);
-    OFF      = (int64_t)C[2] * (1<<16) + ((int64_t)dT * (int64_t)C[4]) / (1<<7);
-    SENS     = (int64_t)C[1] * (1<<15) + ((int64_t)dT * (int64_t)C[3]) / (1<<8);
-
-    temp     = 2000 + (dT * C[6]) / (1<<23);
-    T_MS5837 = (float) temp / 100.0f;                 // result of temperature in deg C in this var
-    press    = (((int64_t)D1 * SENS) / (1<<21) - OFF) / (1<<13);
-    P_MS5837 = (float) press / 10.0f;                 // result of pressure in mBar in this var
+		step = 1; //reset to step 1
+		//return to step 1
+		timeOut1.attach_us(this, &MS5837::Barometer_MS5837, ms5837_wait_time_us);
+		break;
+	default:
+		// if nothing else matches, do the default
+		// default is optional
+		break;
+	}
 }
--- fish/mbed/LPCExpress/mbed_libs/MS5837/MS5837.h
+++ fish/mbed/LPCExpress/mbed_libs/MS5837/MS5837.h
@@ -6,6 +6,7 @@
 #define MS5837_RX_DEPTH 3 //
 #define MS5837_TX_DEPTH 2 //
 
+#define ms5837_wait_time_us 500
 // choose your connection here
 #define ms5837_addr_no_CS  0x76 //0b1110110
 
@@ -29,6 +30,15 @@
 
 #define ms5837_convD2 ms5837_convD2_4096 // choose your sampling rate here
 
+#define ms5837_conv_time_us_256  600 // Conversion Time OSR  256
+#define ms5837_conv_time_us_512  1170 // Conversion Time OSR  512
+#define ms5837_conv_time_us_1024 2280 // Conversion Time OSR 1024
+#define ms5837_conv_time_us_2048 4540 // Conversion Time OSR 2048
+#define ms5837_conv_time_us_4096 9040 // Conversion Time OSR 4096
+#define ms5837_conv_time_us_8192 18080 // Conversion Time OSR 8192
+
+#define ms5837_conv_time_us ms5837_conv_time_us_4096
+
 #define ms5837_ADCread     0x00 // read ADC command
 #define ms5837_PROMread    0xA0 // read PROM command base address
 
@@ -36,24 +46,33 @@ class MS5837{
 private:
     int D1, D2, Temp, C[8];
     float T_MS5837, P_MS5837;
+    int32_t dT, temp;
+    int64_t OFF, SENS, press;
+    int32_t adc;
+    int step;
+    bool started;
     /* Data buffers */
     char ms5837_rx_data[MS5837_RX_DEPTH];
     char ms5837_tx_data[MS5837_TX_DEPTH];
+    Timeout timeOut1;
 
 public:
     MS5837 (PinName sda, PinName scl,
             char ms5837_addr = ms5837_addr_no_CS  )
-            : i2c( sda, scl ), device_address( ms5837_addr << 1 ) {
+            : step(1), started(false), i2c( sda, scl ), device_address( ms5837_addr << 1 ) {
     }
     void MS5837Init(void);
     void MS5837Reset(void);
+    void MS5837Start(void);
+    float MS5837_Pressure (void);
+    float MS5837_Temperature (void);
+protected:
+    void Barometer_MS5837(void);
     void MS5837ReadProm(void);
     void MS5837ConvertD1(void);
     void MS5837ConvertD2(void);
     int32_t MS5837ReadADC(void);
-    float MS5837_Pressure (void);
-    float MS5837_Temperature (void);
-    void Barometer_MS5837(void);
+
 
 
 private:
