ff08dc9419dd29ffadc60c3bd06e477142f6b090
Robert Katzschmann <rkk@csail.mit.edu>
2016-07-05 14:06:56 -0400

    btu: get pressure reading at start

--- fish/mbed/LPCExpress/mbed_libs/BTU/btu.cpp
+++ fish/mbed/LPCExpress/mbed_libs/BTU/btu.cpp
@@ -22,7 +22,9 @@ void BTU::init(float timeStep) {
 
 	}
 	// calculate depth to mbar
-	float pAtmos = 101325;
+	//float pAtmosS1 =  98300; //101325
+	float pAtmosS2 = 101300; //101325
+	float pAtmos = pAtmosS2;
 	float pa_to_mbar = 0.01;
 	m_pAtmosMbar = pAtmos * pa_to_mbar;
 	float densityWater = 1000;
@@ -62,6 +64,9 @@ void BTU::init(float timeStep) {
 	}
 	m_pressureSensor.MS5837Init();
 	m_pressureSensor.MS5837Start();
+	wait(0.1);
+	m_pvDepth = m_pressureSensor.MS5837_Pressure();
+
 }
 
 void BTU::stop() {
