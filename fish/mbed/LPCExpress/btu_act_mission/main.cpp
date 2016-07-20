b12d9c2adef2dc0aabdb88bec061e027edaa19ae
Jeremy Wright <jtwright@mit.edu>
2016-07-20 17:46:36 -0400

    commented btu_act_mission

--- fish/mbed/LPCExpress/btu_act_mission/main.cpp
+++ fish/mbed/LPCExpress/btu_act_mission/main.cpp
@@ -40,28 +40,29 @@ int mode = 2;
 float Kc = 1.0;
 float TauI = 0.0;
 float TauD = 0.0;
-float floorVal;
-float jumpVal;
-float missionFloor;
-float missionStep;
+float floorVal;                 // temp value for mission parameters
+float jumpVal;                  // temp value for mission parameters
+float missionFloor;             // Lowest depth of a mission
+float missionStep;              // Distance between waypoints of a mission
 float setVal = 0.0;             // meters
-Ticker Mission;
-float timeout = 0.0;
-float missionTime = 0.0;
-float successTime = 0.0;
-float missionDepth = 0.0;
-bool missionStarted = false;
-float debriefTime = 0.0;
-bool debriefMode = false;
-FILE *fp;
-bool returnTrip = false;
+Ticker Mission;                 // Ticker for running Mission in parallel with main loop
+float timeout = 0.0;            // time in seconds since last waypoint reached
+float missionTime = 0.0;        // time in seconds that mission has taken
+float successTime = 0.0;        // time in seconds spent within waypoint
+float missionDepth = 0.0;       // current waypoint depth
+bool missionStarted = false;    // boolean detailing whether or not robot has begun automatically following mission parameters
+float debriefTime = 0.0;        // amount of time spent recording log after mission has ended
+bool debriefMode = false;       // boolean indicating whether in debrief mode
+FILE *fp;                       // pointer for writing log
+bool returnTrip = false;        // determines whether the robot is in the descending or ascending part of the mission.  true = ascending
 
+// end a mission and reset the various parameters.
 void terminateMission() {
-    Mission.detach();
-    fclose(fp);
-    counter = 0;
-    TestLED = 0;
-    inMission = 0;
+    Mission.detach();           // stop running mission loop asynchronously
+    fclose(fp);                 // close the file
+    counter = 0;                // resets the LED mission loop counter
+    TestLED = 0;                // turns off the mission loop LED
+    inMission = 0;              // turns off the inMission LED
     timeout = 0.0;
     successTime = 0.0;
     missionTime = 0.0;
@@ -70,9 +71,10 @@ void terminateMission() {
     debriefTime = 0.0;
     returnTrip = false;
 
-    btu.updateAndRunCycle(VELOCITY_CTRL_MODE, UNWOUND_POS);
+    btu.updateAndRunCycle(VELOCITY_CTRL_MODE, UNWOUND_POS); // attempt to return to surface
 }
 
+// returns true if we're close enough to current waypoint
 bool checkThreshold() {
     float error = btu.getDepth() - missionDepth;
     // float error = btu.getServoPos() - missionDepth;
@@ -80,47 +82,61 @@ bool checkThreshold() {
     return (absError <= ERROR_THRESHOLD);
 }
 
+
+// Main mission loop
 void runMission() {
+    // if in debrief mode, update timer and loop again
 	if(debriefMode) {
 		inMission = 0;
 		missionDepth = 0;
 		btu.updateAndRunCycle(VELOCITY_CTRL_MODE, UNWOUND_POS);
+        // finally tidy up and kill loop if debrief time has completed
 		if(debriefTime >= DEBRIEF_TIME_LIMIT) {
 			terminateMission();
 		}
 		debriefTime += TIMESTEP;
-		return;
+		return;                 // start over the mission loop without checking all the later conditions
 	}
+    // counter just for flicking the LED every second
     counter = (counter + 1) % 20;
     if(!counter) {
         TestLED = !TestLED;
     }
+    // start the mission if we're below depth threshold. Does not reset any values.  Cannot end a mission, so running it multiple times is innocuous
     if(btu.getDepth() >= DEPTH_THRESHOLD) {
         inMission = 1;
         missionStarted = true;
         btu.updateMode(DEPTH_CTRL_MODE);
     }
+    // Do nothing if mission has not yet begun
     if(!missionStarted) {
         return;
     }
+    // otherwise run depth controller
     btu.runCycle(missionDepth);
+
+    // if we're close enough to our next waypoint, begin counting success time.  Otherwise, reset success time
     if(checkThreshold()) {
     	successTime += TIMESTEP;
     } else {
     	successTime = 0.0;
     }
+
+    // if we've been near the waypoint for long enough
     if (successTime >= SUCCESS_TIME) {
+        // check if we're completely done.  i.e. done surfacing on our return trip.  If so, report success and begin debriefing
         if((missionDepth <= MIN_MISSION_DEPTH) && (returnTrip == true)) {
             missionSuccess = 1;
             debriefMode = true;
             return;
-        } else if(missionDepth == missionFloor) {
+        } else if(missionDepth == missionFloor) { // otherwise, check if we reached the bottom of our mission and need to begin resurfacing
             returnTrip = true;
             successTime = 0.0;
             missionDepth = utility::clip(missionDepth - missionStep, MIN_MISSION_DEPTH, missionFloor);
             timeout = 0.0;
-        } else {
+        } else {                // otherwise, just advance the mission
         	successTime = 0.0;
+            // if we're on the return trip, move up instead of down
             if(returnTrip) {
                 missionDepth = utility::clip(missionDepth - missionStep, MIN_MISSION_DEPTH, missionFloor);
             } else {
@@ -129,15 +145,19 @@ void runMission() {
             timeout = 0.0;
         }
     }
+    // mission failure.  Taken too long to reach a waypoint.  Begin resurfacing, report failure.
     if (timeout >= MISSION_TIMEOUT) {
         missionSuccess = 0;
         debriefMode = true;
         return;
     }
+    // advance time
     missionTime += TIMESTEP;
     timeout += TIMESTEP;
 }
 
+
+// sets various mission parameters, being sure to detach a mission if it is already present.
 void startMission(float kc, float taui, float taud, float setDepth, float stepDist) {
     terminateMission();
     fp = fopen("/local/log", "w");
@@ -167,27 +187,16 @@ int main() {
   float valueFloats[NUM_FLOATS];
 
   while(1) {
-      // depth = btu.getDepth();
-      // if (mode == DEPTH_CTRL_MODE) {
-      //     pcSerial.printf("m:%d, kc:%f, ti:%f, td:%f, s:%.2f, cu:%.2f, de:%.2f, depth_er:%.4f\r\n",
-      //                 btu.getMode(), btu.getKc(), btu.getTauI(), btu.getTauD(), setVal, btu.getServoPos(), depth, setVal - depth);
-      // } else if (mode == SPEC_POSITION_CTRL_MODE) {
-      //     pcSerial.printf("m:%d, kc:%f, ti:%f, td:%f, s:%.2f, cu:%.2f, de:%.2f, pos_er:%.4f\r\n",
-      //                           btu.getMode(), btu.getKc(), btu.getTauI(), btu.getTauD(), setVal, btu.getServoPos(), depth, setVal - btu.getServoPos());
-      // } else {
-      //     pcSerial.printf("m:%d, s:%.2f, cu:%.2f, de:%.2f\r\n", btu.getMode(), setVal, btu.getServoPos(), depth);
-      // }
-      // pcSerial.printf("m:%d, kc:%f, ti:%f, td:%f, s:%.2f, cu:%.2f, pos_er:%.4f, th:%d, to:%.2f, st:%.2f\r\n", btu.getMode(), btu.getKc(), btu.getTauI(), btu.getTauD(), setVal, btu.getServoPos(), setVal - btu.getServoPos(), checkThreshold(), timeout, successTime);
+      // log in either mission or debrief modes
       if(inMission || debriefMode) {
           float depth = btu.getDepth();
           fprintf(fp, "m:%d, kc:%f, ti:%f, td:%f, s:%.2f, cu:%.2f %.2f, de:%.2f, depth_er:%.4f, time: %.2f, to:%.2f, rt:%d\r\n",
                   btu.getMode(), btu.getKc(), btu.getTauI(), btu.getTauD(), missionDepth, btu.getActPosition(ACT_A), btu.getActPosition(ACT_B), depth, missionDepth - depth, missionTime, timeout, returnTrip);
-      } else {
+      } else {                  // otherwise just try to surface
     	  btu.updateAndRunCycle(VELOCITY_CTRL_MODE, UNWOUND_POS);
       }
-      //float depth = btu.getDepth();
-      //pcSerial.printf("m:%d, kc:%f, ti:%f, td:%f, s:%.2f, cu:%.2f, de:%.2f, depth_er:%.4f, time: %.2f, to:%.2f\r\n",
-      //                  btu.getMode(), btu.getKc(), btu.getTauI(), btu.getTauD(), missionDepth, btu.getServoPos(), depth, missionDepth - depth, missionTime, timeout);
+
+      // take in serial messages to start new missions
       if(serialComm.checkIfNewMessage()) {
           serialComm.getFloats(valueFloats, NUM_FLOATS);
 
