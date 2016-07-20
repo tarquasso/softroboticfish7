d335ed0cf778f4bd070a0645192fdf51486d32ff
Robert Katzschmann <rkk@csail.mit.edu>
2016-07-20 17:14:17 -0400

    only reformat PID Controller code, no functionality change

--- fish/mbed/LPCExpress/mbed_libs/PID/PidController.cpp
+++ fish/mbed/LPCExpress/mbed_libs/PID/PidController.cpp
@@ -1,100 +1,99 @@
 #include "PidController.h"
 #include "utility.h"
 
-PidController::PidController(float Kc, float tauI, float tauD, float interval, float inMin, float inMax, float outMin, float outMax, float b) {
-  setInputLimits(inMin, inMax);
-  setOutputLimits(outMin, outMax);
+PidController::PidController(float Kc, float tauI, float tauD, float interval,
+		float inMin, float inMax, float outMin, float outMax, float b) {
+	setInputLimits(inMin, inMax);
+	setOutputLimits(outMin, outMax);
 
-  sampleTime_ = interval;
+	sampleTime_ = interval;
 
-  setTunings(Kc, tauI, tauD);
+	setTunings(Kc, tauI, tauD);
 
-  reset();
+	reset();
 
 }
 
 void PidController::reset() {
-  setPoint_ = 0.0;
-  processVar_ = 0.0;
-  integral_ = 0.0;
-  errorPrior_ = 0;
-  prevControllerOutput_ = 0.0;
+	setPoint_ = 0.0;
+	processVar_ = 0.0;
+	integral_ = 0.0;
+	errorPrior_ = 0;
+	prevControllerOutput_ = 0.0;
 }
-
-
 
 void PidController::setInputLimits(float inMin, float inMax) {
 
-    //Make sure we haven't been given impossible values.
-    if (inMin >= inMax) {
-        return;
-    }
+	//Make sure we haven't been given impossible values.
+	if (inMin >= inMax) {
+		return;
+	}
 
-    //Rescale the working variables to reflect the changes
+	//Rescale the working variables to reflect the changes
 
-    inMin_  = inMin;
-    inMax_  = inMax;
+	inMin_ = inMin;
+	inMax_ = inMax;
 }
-
 
 void PidController::setOutputLimits(float outMin, float outMax) {
 
-    //Make sure we haven't been given impossible values.
-    if (outMin >= outMax) {
-        return;
-    }
+	//Make sure we haven't been given impossible values.
+	if (outMin >= outMax) {
+		return;
+	}
 
-    //Rescale the working variables to reflect the changes.
-    outMin_  = outMin;
-    outMax_  = outMax;
+	//Rescale the working variables to reflect the changes.
+	outMin_ = outMin;
+	outMax_ = outMax;
 }
 
-
 void PidController::setTunings(float Kc, float tauI, float tauD) {
-  //Verify that the tunings make sense.
-  if (Kc == 0.0 || tauI < 0.0 || tauD < 0.0) {
-    return;
-  }
+	//Verify that the tunings make sense.
+	if (Kc == 0.0 || tauI < 0.0 || tauD < 0.0) {
+		return;
+	}
 
-  if ((Kc != Kc_) || (tauI != tauI_) || (tauD != tauD_)) {
-    reset();
-  }
+	if ((Kc != Kc_) || (tauI != tauI_) || (tauD != tauD_)) {
+		reset();
+	}
 
-  Kc_ = Kc;
-  tauI_ = tauI;
-  tauD_ = tauD;
+	Kc_ = Kc;
+	tauI_ = tauI;
+	tauD_ = tauD;
 }
 
 void PidController::setInterval(float interval) {
-  sampleTime_ = interval;
+	sampleTime_ = interval;
 }
 
 void PidController::setSetPoint(float sp) {
-  setPoint_ = utility::clip(sp, inMin_, inMax_);
+	setPoint_ = utility::clip(sp, inMin_, inMax_);
 }
 
 void PidController::setProcessValue(float pv) {
 
-  processVar_ = utility::clip(pv, inMin_, inMax_);
+	processVar_ = utility::clip(pv, inMin_, inMax_);
 
 }
 
 void PidController::setBias(float b) {
-  bias_ = b;
-  reset();
+	bias_ = b;
+	reset();
 }
 
 float PidController::compute() {
-  float error = setPoint_ - processVar_;
-  // same anti-windup as in the old PID library, but rewritten to handle our bounds
-  if (!(prevControllerOutput_ >= outMax_ && error > 0) && !(prevControllerOutput_ <= outMin_ && error < 0)) {
-    integral_ += (error*sampleTime_);
-  }
-  float derivative = (error - errorPrior_) / sampleTime_;
-  float output = (Kc_ * error) + (tauI_ * integral_) + (tauD_ * derivative) + bias_;
-  errorPrior_ = error;
+	float error = setPoint_ - processVar_;
+	// same anti-windup as in the old PID library, but rewritten to handle our bounds
+	if (!(prevControllerOutput_ >= outMax_ && error > 0)
+			&& !(prevControllerOutput_ <= outMin_ && error < 0)) {
+		integral_ += (error * sampleTime_);
+	}
+	float derivative = (error - errorPrior_) / sampleTime_;
+	float output = (Kc_ * error) + (tauI_ * integral_) + (tauD_ * derivative)
+			+ bias_;
+	errorPrior_ = error;
 
-  prevControllerOutput_ = utility::clip(output, outMin_, outMax_);
+	prevControllerOutput_ = utility::clip(output, outMin_, outMax_);
 
-  return prevControllerOutput_;
+	return prevControllerOutput_;
 }
--- fish/mbed/LPCExpress/mbed_libs/PID/PidController.h
+++ fish/mbed/LPCExpress/mbed_libs/PID/PidController.h
@@ -4,21 +4,24 @@
 #include "mbed.h"
 
 class PidController {
- public:
-  PidController(float Kc, float tauI, float tauD, float interval, float inMin, float inMax, float outMin, float outMax, float bias);
-  void setTunings(float Kc, float tauI, float tauD);
-  void reset();
-  void setSetPoint(float sp);
-  void setProcessValue(float pv);
-  void setBias(float b);
-  float compute();
+public:
+	PidController(float Kc, float tauI, float tauD, float interval, float inMin,
+			float inMax, float outMin, float outMax, float bias);
+	void setTunings(float Kc, float tauI, float tauD);
+	void reset();
+	void setSetPoint(float sp);
+	void setProcessValue(float pv);
+	void setBias(float b);
+	float compute();
 
- private:
-  void setInterval(float interval);
-  void setInputLimits(float inMin, float inMax);
-  void setOutputLimits(float outMin, float outMax);
+private:
+	void setInterval(float interval);
+	void setInputLimits(float inMin, float inMax);
+	void setOutputLimits(float outMin, float outMax);
 
-  float Kc_, tauI_, tauD_, sampleTime_, setPoint_, processVar_, integral_, bias_, errorPrior_, inMin_, inMax_, outMin_, outMax_, prevControllerOutput_;
+	float Kc_, tauI_, tauD_, sampleTime_, setPoint_, processVar_, integral_,
+			bias_, errorPrior_, inMin_, inMax_, outMin_, outMax_,
+			prevControllerOutput_;
 
 };
 
