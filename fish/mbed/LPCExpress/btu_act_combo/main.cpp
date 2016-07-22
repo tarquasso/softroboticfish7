bbd80d090f6f4a4d6c562b6c8f4f55c71f143dbd
Robert Katzschmann <rkk@csail.mit.edu>
2016-07-22 11:27:38 -0400

    working btu_act_combo

--- fish/mbed/LPCExpress/btu_act_combo/main.cpp
+++ fish/mbed/LPCExpress/btu_act_combo/main.cpp
@@ -211,7 +211,7 @@ void runControl() {
 
 void switchModes() {
 	Control.detach();
-	Mission.detach();
+	terminateMission();
 	btu.stop();
 	missionMode = !missionMode;
 	if(!missionMode) {
@@ -224,7 +224,7 @@ void missionCycle() {
 	if(inMission || debriefMode) {
 		float depth = btu.getDepth();
 		fprintf(fp, "m:%d, kc:%f, ti:%f, td:%f, s:%.2f, cu:%.2f %.2f, de:%.2f, depth_er:%.4f, time: %.2f, to:%.2f, rt:%d\r\n",
-				btu.getMode(), btu.getDKc(), btu.getDkI(), btu.getDkD(), missionDepth, btu.getActPosition(ACT_A), btu.getActPosition(ACT_B), depth, missionDepth - depth, missionTime, timeout, returnTrip);
+				btu.getMode(), btu.getDkC(), btu.getDkI(), btu.getDkD(), missionDepth, btu.getActPosition(ACT_A), btu.getActPosition(ACT_B), depth, missionDepth - depth, missionTime, timeout, returnTrip);
 	} else {                  // otherwise just try to surface
 		btu.updateAndRunCycle(VELOCITY_CTRL_MODE, UNWOUND_POS);
 	}
@@ -255,10 +255,10 @@ void testCycle() {
 	depth = btu.getDepth();
 	if (mode == DEPTH_CTRL_MODE) {
 		pcSerial.printf("m:%d, kc:%f, ti:%f, td:%f, s:%.2f, cu:%.2f %.2f, de:%.2f, depth_er:%.4f\r\n",
-				btu.getMode(), btu.getDKc(), btu.getDkI(), btu.getDkD(), setVal, btu.getActPosition(ACT_A), btu.getActPosition(ACT_B), depth, setVal - depth);
+				btu.getMode(), btu.getDkC(), btu.getDkI(), btu.getDkD(), setVal, btu.getActPosition(ACT_A), btu.getActPosition(ACT_B), depth, setVal - depth);
 	} else if (mode == POSITION_CTRL_MODE) {
 		pcSerial.printf("m:%d, kc:%f, ti:%f, td:%f, s:%.2f, cu:%.2f %.2f, de:%.2f, pos_er:%.4f %.4f\r\n",
-				btu.getMode(), btu.getPKc(), btu.getPkI(), btu.getPkD(), setVal, btu.getActPosition(ACT_A), btu.getActPosition(ACT_B), depth, setVal - btu.getActPosition(ACT_A), setVal - btu.getActPosition(ACT_B));
+				btu.getMode(), btu.getPkC(), btu.getPkI(), btu.getPkD(), setVal, btu.getActPosition(ACT_A), btu.getActPosition(ACT_B), depth, setVal - btu.getActPosition(ACT_A), setVal - btu.getActPosition(ACT_B));
 	} else {
 		pcSerial.printf("m:%d, kc:%f, ti:%f, td:%f, s:%.2f, cu:%.2f %.2f, de:%.2f\r\n", btu.getMode(), Kc, TauI, TauD, setVal, btu.getActPosition(ACT_A), btu.getActPosition(ACT_B), depth);
 	}
@@ -286,6 +286,7 @@ int main() {
   btu.init();
   pcSerial.printf("pressure at start: %.6f\r\n", btu.getPressure());
   TestLED = 0;
+  Control.attach(&runControl, TIMESTEP);
 
   while(1) {
 	  if(missionMode) {
