8b467006d91ec6b10defc4e719ea691c92d194e9
Akshat Bubna <akshatb42@gmail.com>
2016-11-04 00:48:53 -0400

    Tested camera successfully with avg RGB values

--- fish/pi/ros/environment.sh
+++ fish/pi/ros/environment.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
-echo "Activating ROS"
-source /opt/ros/indigo/setup.bash
 echo "Setting up PYTHONPATH"
-export PYTHONPATH=/home/ubuntu/softroboticfish6/catkin_ws/src:$PYTHONPATH
+export PYTHONPATH=/home/ubuntu/softroboticfish6/fish/pi/ros/catkin_ws/src:$PYTHONPATH
 echo "Activating development"
-source ~/softroboticfish6/catkin_ws/devel/setup.bash
+source ~/softroboticfish6/fish/pi/ros/catkin_ws/devel/setup.bash
 echo "Setup ROS_HOSTNAME"
 export ROS_HOSTNAME=$HOSTNAME.local
-export FISHTOWN_ROOT=$HOME/softroboticfish6
+export FISHTOWN_ROOT=$HOME/softroboticfish6/fish/pi/ros/
 
-echo "Setting up GCC4MBED_DIR"
-export GCC4MBED_DIR=~/gcc4mbed
-export ROS_LIB_DIR=~/softroboticfish6/mbed/ros_lib
-export GCC4BMED_DEPLOY='cp PROJECT.bin /media/MBED ; sync'
-export PATH=~/gcc4mbed/gcc-arm-none-eabi/bin:$PATH
-
-exec "$@" #Passes arguments. Need this for ROS remote launching to work.
