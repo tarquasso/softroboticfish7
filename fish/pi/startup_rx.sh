f20d434efa33ff75a5ac9cc3ace6e473e9dc55f1
Juan <salazarj@mit.edu>
2019-08-21 13:08:51 -0400

    Uncommented programming, serial logging, added &

--- fish/pi/startup_rx.sh
+++ fish/pi/startup_rx.sh
@@ -1,20 +1,21 @@
 sleep 5
 echo "**** Startup script"
 
-sudo /home/pi/gitRepo/fish/pi/remountMbed.sh
-#sudo python /home/pi/gitRepo/fish/pi/resetMbed.py
+#sudo /home/pi/gitRepo/fish/pi/remountMbed.sh
+sudo python /home/pi/softroboticfish7/fish/pi/resetMbed.py
 
 echo "-- Starting low battery monitor and possibly video"
-sudo python /home/pi/gitRepo/fish/pi/rx_controller.py &
+#sudo python /home/pi/gitRepo/fish/pi/rx_controller.py &
 
 # SERIAL LOGGING (streaming data from mbed)
-#echo "-- Starting serial monitor to record streaming mbed data"
+echo  "-- Starting serial monitor to record streaming mbed data"
 #sudo /home/pi/gitRepo/fish/pi/startup_rx_serialLogging.sh &
 
 # BLUETOOTH JOYSTICK
 echo "-- Starting Joystick Controller"
 sleep 5
-sudo python /home/pi/gitRepo/fish/pi/bluetooth_joystick/BluetoothJoystickController.py &
+sudo python /home/pi/softroboticfish7/fish/pi/bluetooth_joystick/BluetoothJoystickController.py &
 
 echo "- Startup script complete"
 
+exit 0
