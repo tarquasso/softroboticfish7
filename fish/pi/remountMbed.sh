b413824777e3d2a6ac3897c8ef855771bad815bc
Juan <salazarj@mit.edu>
2019-10-06 16:54:43 -0400

    Update remountMbed.sh

--- fish/pi/remountMbed.sh
+++ fish/pi/remountMbed.sh
@@ -1,2 +1,2 @@
-sudo /home/pi/gitRepo/fish/pi/unmountMbed.sh
-sudo /home/pi/gitRepo/fish/pi/mountMbed.sh $1
+sudo /home/pi/softroboticfish7/fish/pi/unmountMbed.sh
+sudo /home/pi/softroboticfish7/fish/pi/mountMbed.sh $1
