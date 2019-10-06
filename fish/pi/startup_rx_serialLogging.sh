a49fc36a249f1afb2254e3aee6e83cfee4305b49
Juan <salazarj@mit.edu>
2019-10-06 16:57:56 -0400

    Update startup_rx_serialLogging.sh

--- fish/pi/startup_rx_serialLogging.sh
+++ fish/pi/startup_rx_serialLogging.sh
@@ -1,4 +1,2 @@
 sleep 10
-sudo python /home/pi/gitRepo/fish/pi/runMbedProgramWithLogging.py startup=0 remount=1 print=0 file='allmydata.wp' > /home/pi/fish/pi/testout.txt &
-
-
+sudo python /home/pi/softroboticfish7/fish/pi/runMbedProgramWithLogging.py startup=0 remount=0 print=0 file='allmydata.wp' > /home/pi/softroboticfish7/fish/pi/testout.txt &
